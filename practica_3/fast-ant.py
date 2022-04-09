f5424602a9685b9eb098bb9a92887095beeb12d7
ddevigner <dedes.mainstream@gmail.com>
2022-04-09 11:26:28 +0200

    Script revisado.

--- practica_3/fast-ant.py
+++ practica_3/fast-ant.py
@@ -5,10 +5,10 @@ user = getpass.getuser()
 target = ""
 if user == "Dede":
     target = "C:\tools\javacc\target"
-elif user == "gabete":
-    target = "/home/gabete/javacc/target"
 elif user == "a780131" or user == "a774631":
-    target = f"/home/{user}/apps/javacc/target"
+    target = "/home/" + user + "/apps/javacc/target"
+else:
+    target = "/home/gabete/javacc/target"
 
-os.system(f"ant -Djavacchome=\"{target}\"")
-os.system("java -jar dist\\adac.jar dist\\test.adac")
+os.system("ant -Djavacchome=\"" + target + "\"")
+os.system("java -jar dist/adac.jar dist/test.adac")
